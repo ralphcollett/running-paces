70c482a01637d9c80505f0441f57b7d7a1e1910b
ralph.collett <ralph.collett@gmail.com>
2021-02-26 12:49:15 +0000

    Remove ignored stuff

--- build/js/packages/running-paces-test/adapter-browser.js
+++
@@ -1,2 +0,0 @@
-require("/Users/ralphwilliamcollett/workspace/running-paces/build/js/node_modules/kotlin-test-js-runner/kotlin-test-karma-runner.js")
-module.exports = require("/Users/ralphwilliamcollett/workspace/running-paces/build/js/packages/running-paces-test/kotlin/running-paces-test.js")
